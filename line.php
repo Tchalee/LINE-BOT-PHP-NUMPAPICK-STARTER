270792343b7b212bb193ab48e775ed4c7947ab45
Tchalee <42532961+Tchalee@users.noreply.github.com>
2018-08-20 15:35:56 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'UY+GPnGQvIiNB69S2dCxhYShAkR6Kuqb8gdG4yzcSfXDgrlC38YBNAKFIEdZeEDJ4OSRokpxtCKAZpU0D42XMD2RQmRR59/FWcxLm+ApRdBPQZPYvIl0N0nXEKJFNKH1Y+KiPLWqua4Zjd0CVoF2fAdB04t89/1O/w1cDnyilFU=
+'; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Ubcab3b6da0ba6b26216151f3b8aa2125',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
